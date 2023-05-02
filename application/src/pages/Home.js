6793f9694a8dcd6ff8431dfcde9162983c72419e
apin014 <78262711+apin014@users.noreply.github.com>
2023-05-02 10:51:02 +0700

    improved Home page

--- application/src/pages/Home.js
+++ application/src/pages/Home.js
@@ -1,20 +1,29 @@
 import React from "react";
 import { Dropdown, Container, Row } from "react-bootstrap";
+import WebFont from "webfontloader";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Home = () => {
+    React.useEffect(() => {
+        WebFont.load({
+          google: {
+            families: ['Kanit', 'Barlow']
+          }
+        });
+      }, []);
+
     return(
-        <div style={{width: '100vw', height: '100vh', backgroundColor: 'lightgreen', paddingTop: '50vh'}}>
-            <Container className="mw-100 p-0" style={{textAlign: 'center'}}>
-                <Row>
+        <div style={{width: '100vw', height: '100vh', backgroundColor: '#77b178', paddingTop: '45vh'}}>
+            <Container className="mw-100 p-0" style={{textAlign: 'center', width: '100%', height: '100%', overflow: 'auto'}}>
+                <Row style={{paddingBottom: '1rem', fontFamily: 'Kanit', fontSize: '2rem', fontWeight: 'bolder'}}>
                     <p1>Welcome to Supermarket Pathfinder!</p1>
                 </Row>
                 <Row>
                     <Dropdown>
-                        <Dropdown.Toggle>
+                        <Dropdown.Toggle style={{fontFamily: 'Barlow', backgroundColor: 'lightyellow', color: 'gray'}}>
                             Select your starting location
                         </Dropdown.Toggle>
-                        <Dropdown.Menu>
+                        <Dropdown.Menu style={{fontFamily: 'Barlow', backgroundColor: 'lightyellow'}}>
                             <Dropdown.Item>Location A</Dropdown.Item>
                             <Dropdown.Item>Location B</Dropdown.Item>
                             <Dropdown.Item>Location C</Dropdown.Item>
